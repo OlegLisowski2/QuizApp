5bc3ae39ab13803fbcdd1ca9870dbc8abf64cb79
Oleg <oleglisowski2@gmail.com>
2025-04-17 17:17:41 +0200

    commit 5

--- app/src/main/java/com/example/quizapp/QuizCreateActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizCreateActivity.kt
@@ -2,16 +2,16 @@ package com.example.quizapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class QuizCreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.quiz_create_page)
 
-        val createButton: Button = findViewById(R.id.btnStartEditing)
-        createButton.setOnClickListener {
+        val btn: MaterialButton = findViewById(R.id.btnStartEditing)
+        btn.setOnClickListener {
             val intent = Intent(this, QuestionCreateActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/example/quizapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/quizapp/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Quizapp2Theme(
+fun QuizAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
